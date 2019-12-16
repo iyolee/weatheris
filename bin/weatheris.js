06cff533a541870175bc2a8c9368e74df53ef2ca
Jenkin Lee <zhongjianlee@outlook.com>
2019-12-17 00:46:46 +0800

    feat: 支持天气预报

--- bin/weatheris.js
+++ bin/weatheris.js
@@ -4,13 +4,21 @@ const program = require('commander')
 const chalk = require('chalk')
 const packageConfig = require('../package.json')
 
-const func = require('../src/index')
+const Service = require('../src/index')
+const log = console.log
 
 program
   .usage('<command> [options]')
+  .option('-c, --city [city name]', 'live weather')
+  .option('-a, --all [city name]', 'live weather and weather forecast')
   .version(packageConfig.version)
-  .option("-c, --city [name]", "add city name")
   .parse(process.argv)
 
-  func.checkParams(process.argv[2])
-  func.getLiveWeather(program.city)
+if (program.city && process.argv[3]) {
+  Service.getLiveWeather(program.city)
+} else if (program.all && process.argv[3]) {
+  Service.getAllWeather(program.all)
+} else {
+  log(chalk.red('Please enter the city name.'))
+  process.exit(1);
+}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 const axios = require('axios')
 const chalk = require('chalk')
 const ora = require('ora')
-const program = require('commander')
 
-const API_URL = "https://restapi.amap.com/v3/weather/weatherInfo"
-const KEY = "dae3073c6347772efe77f8dc288ad221"
+const API_URL = 'https://restapi.amap.com/v3/weather/weatherInfo'
+const KEY = 'dae3073c6347772efe77f8dc288ad221'
 
 const log = console.log
 
-function checkParams(params) {
-  if (params) return
-
-  log()
-  log(chalk.red(`没有找到 ${params} 命令`))
-  log()
-  program.help()
-}
-
 async function getLiveWeather(city) {
-  if (!city) return;
   const spinner = ora('loading...')
   spinner.start()
   try {
@@ -27,15 +16,50 @@ async function getLiveWeather(city) {
     const live = response.data.lives[0]
     log()
     log()
-    log(chalk.yellow(`实况天气发布时间：${live.reporttime}`))
+    log(chalk.yellow('[实况天气]'))
     log(chalk.white(`${live.province} ${live.city}`))
+    log(chalk.white(`发布时间：${live.reporttime}`))
+    log(chalk.blue('---------------'))
     log(`${chalk.cyan('天气：')}${chalk.green(live.weather)}`)
     log(`${chalk.cyan('温度：')}${chalk.green(`${live.temperature}℃`)}`)
     log(`${chalk.cyan('湿度：')}${chalk.green(live.humidity)}`)
     log(`${chalk.cyan('风向：')}${chalk.green(live.winddirection)}`)
     log(`${chalk.cyan('风力：')}${chalk.green(`${live.windpower}级`)}`)
+    log(chalk.blue('---------------'))
   } catch {
+    log(chalk.red('Error: service is not available or city name is incorrect.'))
+  } finally {
+    spinner.stop()
+  }
+}
+
+async function getAllWeather(city) {
+  const spinner = ora('loading...')
+  spinner.start()
+  try {
+    const url = `${API_URL}?city=${encodeURI(city)}&key=${KEY}&extensions=all`
+    const response = await axios.get(url)
+    const forecasts = response.data.forecasts[0]
     log()
+    log()
+    log(chalk.yellow('[天气预报]'))
+    log(chalk.white(`${forecasts.province} ${forecasts.city}`))
+    log(chalk.white(`发布时间：${forecasts.reporttime}`))
+    log(chalk.blue('---------------'))
+    forecasts.casts.forEach(item => {
+    log(`${chalk.cyan('日期：')}${chalk.green(item.date)}`)
+    log(`${chalk.cyan('星期：')}${chalk.green(item.week)}`)
+    log(`${chalk.cyan('白天天气：')}${chalk.green(item.dayweather)}`)
+    log(`${chalk.cyan('晚上天气：')}${chalk.green(item.nightweather)}`)
+    log(`${chalk.cyan('白天温度：')}${chalk.green(`${item.daytemp}℃`)}`)
+    log(`${chalk.cyan('晚上温度：')}${chalk.green(`${item.nighttemp}℃`)}`)
+    log(`${chalk.cyan('白天风向：')}${chalk.green(item.daywind)}`)
+    log(`${chalk.cyan('晚上风向：')}${chalk.green(item.nightwind)}`)
+    log(`${chalk.cyan('白天风力：')}${chalk.green(`${item.daypower}级`)}`)
+    log(`${chalk.cyan('晚上风力：')}${chalk.green(`${item.nightpower}级`)}`)
+    log(chalk.blue('---------------'))
+    })
+  } catch {
     log(chalk.red('Error: service is not available or city name is incorrect.'))
   } finally {
     spinner.stop()
@@ -44,5 +68,5 @@ async function getLiveWeather(city) {
 
 module.exports = {
   getLiveWeather,
-  checkParams
+  getAllWeather
 };
